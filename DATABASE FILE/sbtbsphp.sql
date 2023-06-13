f1beb3123cea4d6feea4a1463d8843a827e75244
rdonkdev <hthembwe46@gmail.com>
2023-06-13 11:46:12 +0200

    Info update

--- DATABASE FILE/sbtbsphp.sql
+++ DATABASE FILE/sbtbsphp.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2021 at 06:02 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Tempo de geração: 13-Jun-2023 às 11:43
+-- Versão do servidor: 10.4.28-MariaDB
+-- versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sbtbsphp`
+-- Banco de dados: `sbtbsphp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookings`
+-- Estrutura da tabela `bookings`
 --
 
 CREATE TABLE `bookings` (
@@ -34,38 +35,34 @@ CREATE TABLE `bookings` (
   `customer_route` varchar(200) NOT NULL,
   `booked_amount` int(100) NOT NULL,
   `booked_seat` varchar(100) NOT NULL,
-  `booking_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `booking_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `bookings`
+-- Extraindo dados da tabela `bookings`
 --
 
 INSERT INTO `bookings` (`id`, `booking_id`, `customer_id`, `route_id`, `customer_route`, `booked_amount`, `booked_seat`, `booking_created`) VALUES
-(60, 'TBZJ360', 'CUST-2114034', 'RT-1908653', 'CITY1 &rarr; CITY2', 100, '3', '2021-10-16 22:15:13'),
-(61, 'QK0MT61', 'CUST-2017936', 'RT-9941455', 'EDROISCHESTER &rarr; BRUGOW', 110, '15', '2021-10-17 22:36:10'),
-(62, 'A8L5662', 'CUST-5585037', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '2', '2021-10-18 00:08:51'),
-(63, 'QDNGC63', 'CUST-8996235', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '15', '2021-10-18 09:31:30'),
-(64, 'X34RW64', 'CUST-9474738', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '6', '2021-10-18 09:32:21'),
-(65, 'JKZVT65', 'CUST-4031139', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '18', '2021-10-18 09:33:36'),
-(66, 'HIIAN66', 'CUST-9997540', 'RT-5887160', 'FLORIA &rarr; ARKBY', 118, '16', '2021-10-18 09:40:16'),
-(67, 'QLOE167', 'CUST-9997540', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '12', '2021-10-18 09:41:01');
+(70, '6HRUS70', 'CUST-2114034', 'RT-6028759', 'MAPUTO &rarr; INHAMBANE', 600, '35', '2023-06-13 11:19:30'),
+(71, 'MPO4X71', 'CUST-2017936', 'RT-1908653', 'MAPUTO &rarr; NIASSA', 5500, '11', '2023-06-13 11:22:29'),
+(72, 'RVVXO72', 'CUST-8996235', 'RT-3835554', 'MAPUTO &rarr; CABODELGADO', 3500, '26', '2023-06-13 11:24:00'),
+(73, 'PEWEJ73', 'CUST-9997540', 'RT-5887160', 'MAPUTO &rarr; GAZA', 250, '18', '2023-06-13 11:24:33');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buses`
+-- Estrutura da tabela `buses`
 --
 
 CREATE TABLE `buses` (
   `id` int(100) NOT NULL,
   `bus_no` varchar(255) NOT NULL,
-  `bus_assigned` tinyint(1) NOT NULL DEFAULT '0',
-  `bus_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `bus_assigned` tinyint(1) NOT NULL DEFAULT 0,
+  `bus_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `buses`
+-- Extraindo dados da tabela `buses`
 --
 
 INSERT INTO `buses` (`id`, `bus_no`, `bus_assigned`, `bus_created`) VALUES
@@ -84,7 +81,7 @@ INSERT INTO `buses` (`id`, `bus_no`, `bus_assigned`, `bus_created`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Estrutura da tabela `customers`
 --
 
 CREATE TABLE `customers` (
@@ -92,26 +89,26 @@ CREATE TABLE `customers` (
   `customer_id` varchar(255) NOT NULL,
   `customer_name` varchar(30) NOT NULL,
   `customer_phone` varchar(10) NOT NULL,
-  `customer_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `customer_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `customers`
+-- Extraindo dados da tabela `customers`
 --
 
 INSERT INTO `customers` (`id`, `customer_id`, `customer_name`, `customer_phone`, `customer_created`) VALUES
-(34, 'CUST-2114034', 'Dfirst Dlast', '7002001200', '2021-10-16 22:09:12'),
-(35, 'CUST-8996235', 'Willian Hobbs', '4012222222', '2021-10-17 22:30:23'),
-(36, 'CUST-2017936', 'George Watts', '7011111111', '2021-10-17 22:30:53'),
-(37, 'CUST-5585037', 'Bobb Horn', '1111111110', '2021-10-17 22:31:20'),
-(38, 'CUST-9474738', 'Alan Moore', '7900000000', '2021-10-18 09:32:02'),
-(39, 'CUST-4031139', 'Jamie Rhoades', '1003000010', '2021-10-18 09:33:08'),
-(40, 'CUST-9997540', 'Demo Customer', '7777777700', '2021-10-18 09:39:10');
+(34, 'CUST-2114034', 'Kelvin Muteto', '860000000', '2021-10-16 22:09:12'),
+(35, 'CUST-8996235', 'Blest Dz', '850000000', '2021-10-17 22:30:23'),
+(36, 'CUST-2017936', 'Jamal Ahade', '830000000', '2021-10-17 22:30:53'),
+(37, 'CUST-5585037', 'Jorge Eurico', '870000000', '2021-10-17 22:31:20'),
+(38, 'CUST-9474738', 'Alan Julião', '840000000', '2021-10-18 09:32:02'),
+(39, 'CUST-4031139', 'Antonio Fiscal', '820000000', '2021-10-18 09:33:08'),
+(40, 'CUST-9997540', 'Snaylon Niggga', '847777777', '2021-10-18 09:39:10');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `routes`
+-- Estrutura da tabela `routes`
 --
 
 CREATE TABLE `routes` (
@@ -122,47 +119,47 @@ CREATE TABLE `routes` (
   `route_dep_date` date NOT NULL,
   `route_dep_time` time NOT NULL,
   `route_step_cost` int(100) NOT NULL,
-  `route_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `route_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `routes`
+-- Extraindo dados da tabela `routes`
 --
 
 INSERT INTO `routes` (`id`, `route_id`, `bus_no`, `route_cities`, `route_dep_date`, `route_dep_time`, `route_step_cost`, `route_created`) VALUES
-(53, 'RT-1908653', 'MVL1000', 'CITY1,CITY2', '2021-10-17', '22:05:00', 100, '2021-10-16 22:05:42'),
-(54, 'RT-3835554', 'MMM9969', 'ZEKA,ZREGOW', '2021-10-19', '23:13:00', 70, '2021-10-16 22:12:32'),
-(55, 'RT-9941455', 'RDH4255', 'EDROISCHESTER,BRUGOW', '2021-10-18', '10:00:00', 110, '2021-10-17 22:34:47'),
-(56, 'RT-9069556', 'XYZ7890', 'ANTALAND,ZREGOW', '2021-10-19', '11:40:00', 85, '2021-10-17 23:39:57'),
-(57, 'RT-775557', 'ABC0010', 'ENCEFORD,VLIRGINIA', '2021-10-19', '13:30:00', 131, '2021-10-17 23:42:12'),
-(58, 'RT-753558', 'TTH8888', 'ARKBY,VEIM', '2021-10-20', '12:04:00', 55, '2021-10-18 00:04:42'),
-(59, 'RT-6028759', 'LLL7699', 'BELRITH,ARKBY', '2021-10-20', '13:50:00', 166, '2021-10-18 00:07:50'),
-(60, 'RT-5887160', 'CAS3300', 'FLORIA,ARKBY', '2021-10-19', '10:30:00', 118, '2021-10-18 09:38:30');
+(53, 'RT-1908653', 'MVL1000', 'MAPUTO,NIASSA', '2023-06-14', '22:05:00', 5500, '2021-10-16 22:05:42'),
+(54, 'RT-3835554', 'MMM9969', 'MAPUTO,CABODELGADO', '2023-06-14', '23:13:00', 3500, '2021-10-16 22:12:32'),
+(55, 'RT-9941455', 'RDH4255', 'MAPUTO,NAMPULA', '2023-06-14', '10:00:00', 3000, '2021-10-17 22:34:47'),
+(56, 'RT-9069556', 'XYZ7890', 'MAPUTO,TETE', '2023-06-13', '15:40:00', 2500, '2021-10-17 23:39:57'),
+(57, 'RT-775557', 'ABC0010', 'MAPUTO-MANICA', '2023-06-14', '04:30:00', 1250, '2021-10-17 23:42:12'),
+(58, 'RT-753558', 'TTH8888', 'MAPUTO,ZAMBEZIA', '2023-06-13', '16:04:00', 1200, '2021-10-18 00:04:42'),
+(59, 'RT-6028759', 'LLL7699', 'MAPUTO,INHAMBANE', '2023-06-13', '13:50:00', 600, '2021-10-18 00:07:50'),
+(60, 'RT-5887160', 'CAS3300', 'MAPUTO,GAZA', '2023-06-14', '06:30:00', 250, '2021-10-18 09:38:30');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `seats`
+-- Estrutura da tabela `seats`
 --
 
 CREATE TABLE `seats` (
   `bus_no` varchar(155) NOT NULL,
   `seat_booked` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `seats`
+-- Extraindo dados da tabela `seats`
 --
 
 INSERT INTO `seats` (`bus_no`, `seat_booked`) VALUES
 ('ABC0010', NULL),
 ('BCC9999', NULL),
-('CAS3300', '16'),
-('LLL7699', NULL),
-('MMM9969', '2,15,6,18,12'),
-('MVL1000', '3'),
+('CAS3300', '18'),
+('LLL7699', '35'),
+('MMM9969', '26'),
+('MVL1000', '11'),
 ('NBS4455', NULL),
-('RDH4255', '15'),
+('RDH4255', ''),
 ('SSX6633', NULL),
 ('TTH8888', NULL),
 ('XYZ7890', NULL);
@@ -170,7 +167,7 @@ INSERT INTO `seats` (`bus_no`, `seat_booked`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estrutura da tabela `users`
 --
 
 CREATE TABLE `users` (
@@ -178,86 +175,93 @@ CREATE TABLE `users` (
   `user_fullname` varchar(100) NOT NULL,
   `user_name` varchar(30) NOT NULL,
   `user_password` varchar(255) NOT NULL,
-  `user_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `user_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Extraindo dados da tabela `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_fullname`, `user_name`, `user_password`, `user_created`) VALUES
-(1, 'Liam Moore', 'admin', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', '2021-06-02 13:55:21'),
-(2, 'Test Admin', 'testadmin', '$2y$10$A2eGOu1K1TSBqMwjrEJZg.lgy.FmCUPl/l5ugcYOXv4qKWkFEwcqS', '2021-10-17 21:10:07');
+(1, 'Nelio Adelino', 'admin', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', '2021-06-02 13:55:21'),
+(2, 'Test Admin', 'testadmin', '$2y$10$A2eGOu1K1TSBqMwjrEJZg.lgy.FmCUPl/l5ugcYOXv4qKWkFEwcqS', '2021-10-17 21:10:07'),
+(3, 'Nelio Adelino', 'snaylon', '$2y$10$PXHM5iQRBb9uw6Ss.sA6/eZtlomgNHmNKT4TPd56ysBnGHXLgO4oC', '2023-06-13 11:07:01');
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `bookings`
+-- Índices para tabela `bookings`
 --
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `buses`
+-- Índices para tabela `buses`
 --
 ALTER TABLE `buses`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `customers`
+-- Índices para tabela `customers`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `routes`
+-- Índices para tabela `routes`
 --
 ALTER TABLE `routes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `seats`
+-- Índices para tabela `seats`
 --
 ALTER TABLE `seats`
   ADD PRIMARY KEY (`bus_no`);
 
 --
--- Indexes for table `users`
+-- Índices para tabela `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `bookings`
+-- AUTO_INCREMENT de tabela `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+
 --
--- AUTO_INCREMENT for table `buses`
+-- AUTO_INCREMENT de tabela `buses`
 --
 ALTER TABLE `buses`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT de tabela `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
 --
--- AUTO_INCREMENT for table `routes`
+-- AUTO_INCREMENT de tabela `routes`
 --
 ALTER TABLE `routes`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
